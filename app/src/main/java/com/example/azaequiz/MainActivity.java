833a997c0e4719a299f54e9fe34e6c33b3b118f6
Yoon Changho <ho9938@unist.ac.kr>
2023-06-05 02:26:19 +0900

    implement TextScanner

--- app/src/main/java/com/example/azaequiz/MainActivity.java
+++ app/src/main/java/com/example/azaequiz/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -29,91 +30,33 @@ import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    static final int REQUEST_CODE = 2;
-
-    ImageView imageView;    // 갤러리에서 가져온 이미지를 보여줄 뷰
-    Uri uri;                // 갤러리에서 가져온 이미지에 대한 Uri
-    Bitmap bitmap;          // 갤러리에서 가져온 이미지를 담을 비트맵
-    InputImage image;       // ML 모델이 인식할 인풋 이미지
-    TextView textInfo;     // ML 모델이 인식한 텍스트를 보여줄 뷰
-    Button getImage, changeToText;  // 이미지 가져오기 버튼, 이미지 인식 버튼
-    TextRecognizer recognizer;    //텍스트 인식에 사용될 모델
+    private TextScanner textScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-        textInfo = findViewById(R.id.textInfo);
-        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);    //텍스트 인식에 사용될 모델
-
-        // GET IMAGE 버튼
-        getImage = findViewById(R.id.getImage);
-        getImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(intent, REQUEST_CODE);
-            }
-        });
-
-        // IMAGE DETECTION 버튼
-        changeToText = findViewById(R.id.changeToText);
-        changeToText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextRecognition(recognizer);
-            }
-        });
+        textScanner = new TextScanner(recogSuccess, recogFailure);
     }
 
-    @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if (requestCode == REQUEST_CODE) {
-            // 갤러리에서 선택한 사진에 대한 uri를 가져온다.
-            uri = data.getData();
+    public void notifyText(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
 
-            setImage(uri);
+    OnSuccessListener<Text> recogSuccess = new OnSuccessListener<Text>() {
+        @Override
+        public void onSuccess(Text text) {
+            Log.d("recogBitmap", "success");
+            String result = text.getText();
+            notifyText(result);
         }
-    }
+    };
 
-    // uri를 비트맵으로 변환시킨후 이미지뷰에 띄워주고 InputImage를 생성하는 메서드
-    private void setImage(Uri uri) {
-        try{
-            InputStream in = getContentResolver().openInputStream(uri);
-            bitmap = BitmapFactory.decodeStream(in);
-            imageView.setImageBitmap(bitmap);
-
-            image = InputImage.fromBitmap(bitmap, 0);
-            Log.e("setImage", "이미지 to 비트맵");
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
+    OnFailureListener recogFailure = new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            Log.e("recogBitmap", "fail");
         }
-    }
-
-    private void TextRecognition(TextRecognizer recognizer){
-        recognizer.process(image)
-                // 이미지 인식에 성공하면 실행되는 리스너
-                .addOnSuccessListener(new OnSuccessListener<Text>() {
-                    @Override
-                    public void onSuccess(Text visionText) {
-                        Log.e("텍스트 인식", "성공");
-                        // Task completed successfully
-                        String resultText = visionText.getText();
-                        textInfo.setText(resultText);  // 인식한 텍스트를 TextView에 세팅
-                    }
-                })
-                // 이미지 인식에 실패하면 실행되는 리스너
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e("텍스트 인식", "실패: " + e.getMessage());
-                            }
-                        });
-    }
+    };
 }
